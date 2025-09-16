62e7bc399fb9237091b3ef99d1fbc1ce2ce3fc88
Hamid Bersi <hamidbersi1@gmail.com>
2025-09-16 02:57:10 +0200

    add xss and JWT_KEY

--- Server/src/Routes/ordersRoutes.ts
+++ Server/src/Routes/ordersRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import {
   createOrder,
+  deleteOrder,
   getAllOrders,
   getOrderById,
+  updateOrderStatus,
 } from "../controllers/ordersController.js";
 import { validate, idParamsSchema } from "../validation/validate.js";
 import { get } from "http";
@@ -12,5 +14,7 @@ const router = Router();
 router.post("/", createOrder);
 router.get("/", getAllOrders);
 router.get("/:id", getOrderById);
+router.patch("/:id", updateOrderStatus);
+router.delete("/:id", deleteOrder);
 
 export default router;
