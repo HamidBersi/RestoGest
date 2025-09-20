7f6b1d36b1059dbb9462b37164b85f65fba89c50
Hamid Bersi <hamidbersi1@gmail.com>
2025-09-20 14:36:50 +0200

    handle tailwindcss issue && use tailwind v4 instead

--- Client/src/Components/Header.tsx
+++ Client/src/Components/Header.tsx
@@ -1,6 +1,14 @@
+import SloganIcon from "./SloganIcon";
+
 const Header = () => {
   return;
-  <div></div>;
+  <div className="flex items-center justify-between p-4 bg-gray-100">
+    <div>
+      <SloganIcon />
+      <h1 className="text-3xl font-bold">RestoGest</h1>
+      <p>Votre assistant cuisine professionnel</p>
+    </div>
+  </div>;
 };
 
 export default Header;
